790c0376eedfeef21bb498eee36a9c810864b3ff
namanthecoder01 <namane218@gmail.com>
2025-07-07 18:12:54 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import { createRequire } from "module";
 import axios from "axios";
 
 // Remote compiler-backend URL (can be set via env variable)
-const COMPILER_BACKEND_URL = process.env.COMPILER_BACKEND_URL || "http://13.53.112.15:8000";
+const COMPILER_BACKEND_URL = process.env.COMPILER_BACKEND_URL || "https://online-judge-algorave-2.onrender.com";
 
 // Database and configuration imports
 import { DBConnection } from "./database/db.js";
